074627b7de1ebb526578c40b6456b0c55005962b
ArrayDPS <xzwy128@gmail.com>
2025-01-29 20:58:05 -0600

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 conda create -n arraydps python=3.8
 conda activate arraydps
-pip install torch==1.11.0+cu113 torch vision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install -r requirement.txt
